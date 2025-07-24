5114060811733e7dadf6a50a91fad3d44a570cbb
NVD0606 <nguyenvandat0611@gmail.com>
2025-07-24 19:17:16 +0700

    function sendmail

--- js/1988012.js
+++ js/1988012.js
@@ -1,4 +1,5 @@
 const API = 'https://web1-api.vercel.app/api';
+const AUTHENTICATE_API = 'https://web1-api.vercel.app/users';
 
 async function loadData(request, templateId, viewId) {
         const response = await fetch(`${API}/${request}`);
@@ -16,3 +17,21 @@ async function loadData(request, templateId, viewId) {
         // console.log(view)
         view.innerHTML = html;
       };
+
+async function getAuthenticateToken(username, password) {
+        let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify({ username, password })
+        });
+
+        if (!response.ok) {
+          throw new Error('Authentication failed');
+        }
+
+        const data = await response.json();
+        return data.token;
+      }
